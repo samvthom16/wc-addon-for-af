59931e8a6493f5c8ba3de40f7fdf0ab7023da018
Samuel Thomas <samvthom16@gmail.com>
2022-07-12 13:12:53 +0530

    refactor pdf - prepare address field

--- lib/functions.php
+++ lib/functions.php
@@ -99,9 +99,9 @@ function af_combine_field( $fields, $data, $seperator = "\r\n" ){
 
 function af_data_invoice_statement( $data ){
 
-  $data['city_state'] = af_combine_field( array( 'city', 'state' ), $data, ', ' );
+  //$data['city_state'] = af_combine_field( array( 'city', 'state' ), $data, ', ' );
   $data['country'] = af_combine_field( array( 'country', 'code_postal' ), $data, ', ' );
-  $data['address_information'] = af_combine_field( array( 'primary_address', 'secondary_address', 'city_state', 'country' ), $data );
+  $data['address_information'] = af_combine_field( array( 'primary_address', 'secondary_address', 'city', 'state', 'country' ), $data );
 
   $data['delivery_title'] = af_combine_field( array( 'date_start', 'delivery_place' ), $data, ', ' );
   $data['delivery_remark'] = $data['delivery_title'] . "\r\n\r\n" . $data['delivery_place_remark'];
